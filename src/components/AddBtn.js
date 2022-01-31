c52f203d14ef6effd092be3272dc5159014d9675
Junaid <junaidarshad113@gmail.com>
2022-01-31 19:02:18 +0500

    Worked on React configuration,scaffolding, navbar,textfield,button and footer in react task completed

---
+++ src/components/AddBtn.js
@@ -0,0 +1,7 @@
+ const AddBtn = ()=>{
+     return(
+        <button type="button" className="btn btn-secondary">Add Button</button>
+     );
+
+ }
+ export default AddBtn;
---
+++ src/components/InputField.js
@@ -0,0 +1,16 @@
+const InputField=()=>{
+
+    return(
+        <div className="input-group mb-3">
+  
+  <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1"/>
+  <input type="text" className="form-control" placeholder="Password" aria-label="Username" aria-describedby="basic-addon1"/>
+</div>
+
+
+
+
+    );
+
+}
+export default InputField;
